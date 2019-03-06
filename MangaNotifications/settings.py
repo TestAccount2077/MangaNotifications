0a5def0428460ed012d901c8932cf1f48a33ccda
TestAccount2077 <maselectronics594@gmail.com>
2019-03-06 07:45:47 +0200

    Update settings.py

--- MangaNotifications/settings.py
+++ MangaNotifications/settings.py
@@ -120,3 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+if DEBUG:
+    pass
+
+else:
+    
+    import dj_database_url
+    
+    DATABASES['default'] = dj_database_url.config()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
